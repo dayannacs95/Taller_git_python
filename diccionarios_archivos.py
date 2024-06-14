b539ecfffa7b5cacbee5f5aeccbb6ab8ae43dbe2
Kmoyon55 <163060563+Kmoyon55@users.noreply.github.com>
2024-06-14 13:48:01 -0500

    Añadí el archivo diccionarios_archivos.py

---
+++ diccionarios_archivos.py
@@ -0,0 +1,23 @@
+def leer_archivo(nombre_archivo):
+    diccionario_palabras = {}
+    archivo=open(nombre_archivo,"r",encoding="utf-8")
+    for linea in archivo:
+        palabras = linea.split()
+        for palabra in palabras:
+            palabra = palabra.lower().strip('.,;:"\'()!?')
+            if palabra in diccionario_palabras:
+                diccionario_palabras[palabra] += 1
+            else:
+                diccionario_palabras[palabra] = 1
+    return diccionario_palabras
+
+def guardar_diccionario(diccionario, nuevo_archivo):
+    archivo2=open(nuevo_archivo,"w",encoding="utf-8")
+    for palabra, frecuencia in diccionario.items():
+        archivo2.write(f'{palabra}: {frecuencia}\n')
+
+""""
+diccpalabras=leer_archivo("textogithub.txt")
+print(primera)
+archivotxt=guardar_diccionario(diccpalabras, "diccionario.txt")
+"""
